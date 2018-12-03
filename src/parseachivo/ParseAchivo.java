cc308cb3d42ea22a92ac0afea6884a4bac891a86
Gonzalo Verussa <gonzaloverussa@gmail.com>
2018-12-02 21:38:45 -0300

    agregados algunos metodos

--- src/parseachivo/ParseAchivo.java
+++ src/parseachivo/ParseAchivo.java
@@ -9,17 +9,11 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
-import java.nio.charset.Charset;
-import java.nio.charset.CharsetEncoder;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 /**
  *
@@ -31,59 +25,177 @@ public class ParseAchivo {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-        String filePath = args[0];
-        BufferedReader fileReader = null;
-        String nombreArchivoSalida = "src/set_verbs_sin_null_suff_reg.arff";
-        //FileWriter escritorArchivo = new FileWriter(nombreArchivoSalida);
-        BufferedWriter bufferEscritura = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(nombreArchivoSalida), "Windows-1252"));
-        //BufferedWriter bufferEscritura = new BufferedWriter(escritorArchivo);
-        String[] inst;
-        try {
-            FileInputStream is = new FileInputStream(filePath);
-            InputStreamReader isr = new InputStreamReader(is, "Windows-1252");
-            fileReader = new BufferedReader(isr);
-            String currentLineString = fileReader.readLine();
-            //preambulo
-            while (!currentLineString.contains("@data")) {
-                bufferEscritura.write(currentLineString + "\n");
-                currentLineString = fileReader.readLine();
-            }
-            bufferEscritura.write("instancia[1].equalsIgnoreCase(\"ANY\") \n");
-            bufferEscritura.write(currentLineString + "\n");
-            //seccion de datos
-            currentLineString = fileReader.readLine();
-            while (currentLineString != null) {
-                inst = currentLineString.split(",");
-                if (condicion(inst)) {
-                    bufferEscritura.write(currentLineString + "\n");
-                } else {
-                    System.out.println("elimina: " + currentLineString);
-                }
-                currentLineString = fileReader.readLine();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (fileReader != null) {
-                    // close reader, good practice
-                    bufferEscritura.close();
-                    fileReader.close();
-                }
-            } catch (IOException ex) {
-            }
-        }
+	ArrayList<String[]> dataset = new ArrayList<>();
+	String filePath = args[0];
+	BufferedReader fileReader = null;
+	String nombreArchivoSalida = "src/set_verbs_irreg.arff";
+	//FileWriter escritorArchivo = new FileWriter(nombreArchivoSalida);
+	BufferedWriter bufferEscritura = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(nombreArchivoSalida), "Windows-1252"));
+	//BufferedWriter bufferEscritura = new BufferedWriter(escritorArchivo);
+	String[] inst;
+	try {
+	    FileInputStream is = new FileInputStream(filePath);
+	    InputStreamReader isr = new InputStreamReader(is, "Windows-1252");
+	    fileReader = new BufferedReader(isr);
+	    String currentLineString = fileReader.readLine();
+	    //preambulo
+	    while (!currentLineString.contains("@data")) {
+		bufferEscritura.write(currentLineString + "\n");
+		currentLineString = fileReader.readLine();
+	    }
+	    bufferEscritura.write(currentLineString + "\n");
+	    //seccion de datos
+	    currentLineString = fileReader.readLine();
+	    while (currentLineString != null) {
+		inst = currentLineString.split(",");
+		dataset.add(inst);
+		currentLineString = fileReader.readLine();
+	    }
+	    deleteDuplicate(dataset);
+	    dataset = getIrregulares(dataset);
+	    LinkedList<String> datasetToString = new LinkedList<>();
+	    String lineToString;
+	    for (String[] line : dataset) {
+		lineToString = "";
+		for (int i = 0; i < line.length; i++) {
+		    lineToString += line[i] + ",";
+		}
+		lineToString = lineToString.substring(0, lineToString.length() - 1);
+		datasetToString.add(lineToString);
+	    }
+	    for (String line : datasetToString) {
+		bufferEscritura.write(line + "\n");
+	    }
+
+	} catch (IOException e) {
+	    e.printStackTrace();
+	} finally {
+	    try {
+		if (fileReader != null) {
+		    // close reader, good practice
+		    bufferEscritura.close();
+		    fileReader.close();
+		}
+	    } catch (IOException ex) {
+	    }
+	}
     }
 
-    /**
-     * Condicion devuelve true si esa instancia debe ir en el nuevo dataset.
-     * False en caso contrario.
-     *
-     * @param instancia
-     * @return
-     */
-    public static boolean condicion(String[] instancia) {
-        boolean res = (instancia[1].equalsIgnoreCase("ANY"));
-        return res;
+    public static ArrayList getRegulares(ArrayList<String[]> dataset) {
+	String[] element = dataset.get(0), element2, nextElement, nextElement2;
+	while (element != null) {
+	    if (dataset.indexOf(element) + 1 < dataset.size()) {
+		nextElement = dataset.get(dataset.indexOf(element) + 1);
+	    } else {
+		nextElement = null;
+	    }
+	    if (!element[9].equalsIgnoreCase("ANY") || (!element[1].equalsIgnoreCase("ANY") && !element[7].startsWith(element[1]))) {
+		dataset.remove(element);
+//		System.out.print("elimina: ");
+//		for (int i = 0; i < element.length; i++) {
+//		    System.out.print(element[i] + " ");
+//		}
+//		System.out.println("");
+		element2 = dataset.get(0);
+		while (element2 != null) {
+		    if (dataset.indexOf(element2) + 1 < dataset.size()) {
+			nextElement2 = dataset.get(dataset.indexOf(element2) + 1);
+		    } else {
+			nextElement2 = null;
+		    }
+		    if (element2[0].equalsIgnoreCase(element[0]) && element2[1].equalsIgnoreCase(element[1]) && element2[2].equalsIgnoreCase(element[2])) {
+			if (nextElement == element2) {
+			    nextElement = nextElement2;
+			}
+			dataset.remove(element2);
+//			System.out.print("por ende elimina: ");
+//			for (int i = 0; i < element2.length; i++) {
+//			    System.out.print(element2[i] + " ");
+//			}
+//			System.out.println("");
+		    }
+		    element2 = nextElement2;
+		}
+	    }
+	    element = nextElement;
+	}
+
+	return getColumn(dataset, new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8});
+    }
+
+    public static ArrayList getColumn(ArrayList<String[]> dataset, int[] columns) {
+	//se debe eliminar las columnas en el encabezado
+	ArrayList<String[]> newDataSet = new ArrayList<>();
+	String[] inst;
+	for (int i = 0; i < dataset.size(); i++) {
+	    inst = new String[columns.length];
+	    for (int j = 0; j < columns.length; j++) {
+		inst[j] = dataset.get(i)[columns[j]];
+	    }
+	    newDataSet.add(inst);
+	}
+	return newDataSet;
+    }
+
+    public static ArrayList getIrregulares(ArrayList<String[]> dataset) {
+	ArrayList<String[]> regularDataset = getRegulares((ArrayList<String[]>) dataset.clone()), newDataset = new ArrayList<>();
+	boolean encontro;
+	boolean igual;
+	String[] line2;
+	for (String[] line : dataset) {
+	    line2 = regularDataset.get(0);
+	    encontro = false;
+	    while (!encontro && line2 != null) {
+		int i = 0;
+		igual = true;
+		while (i < line2.length && igual) {
+		    igual = (line[i].equalsIgnoreCase(line2[i]));
+		    i++;
+		}
+		encontro = igual;
+		if (regularDataset.indexOf(line2) + 1 < regularDataset.size()) {
+		    line2 = regularDataset.get(regularDataset.indexOf(line2) + 1);
+		} else {
+		    line2 = null;
+		}
+	    }
+	    if (!encontro) {
+		newDataset.add(line);
+	    }
+	}
+	return newDataset;
+    }
+
+    public static ArrayList deleteDuplicate(ArrayList<String[]> dataset) {
+	String[] element = dataset.get(0), element2, nextElement, nextElement2;
+	while (element != null) {
+	    if (dataset.indexOf(element) + 1 < dataset.size()) {
+		nextElement = dataset.get(dataset.indexOf(element) + 1);
+	    } else {
+		nextElement = null;
+	    }
+	    element2 = nextElement;
+	    while (element2 != null) {
+		if (dataset.indexOf(element2) + 1 < dataset.size()) {
+		    nextElement2 = dataset.get(dataset.indexOf(element2) + 1);
+		} else {
+		    nextElement2 = null;
+		}
+		if (element2[0].equalsIgnoreCase(element[0]) && element2[1].equalsIgnoreCase(element[1]) && element2[2].equalsIgnoreCase(element[2]) && element2[3].equalsIgnoreCase(element[3]) && element2[4].equalsIgnoreCase(element[4]) && element2[5].equalsIgnoreCase(element[5]) && element2[6].equalsIgnoreCase(element[6])) {
+		    if (nextElement == element2) {
+			nextElement = nextElement2;
+		    }
+		    dataset.remove(element2);
+		    System.out.print("elimina duplicado: ");
+		    for (int i = 0; i < element2.length; i++) {
+			System.out.print(element2[i] + " ");
+		    }
+		    System.out.println("");
+		}
+		element2 = nextElement2;
+	    }
+	    element = nextElement;
+	}
+	return dataset;
     }
 }
